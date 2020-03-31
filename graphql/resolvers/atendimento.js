5689a9c6d8d49f69bd6e223a9cefa21e7bfcd2d3
Henrique Melanda <hjdesigner23@gmail.com>
2020-03-31 11:43:09 -0300

    Creating our service CRUD

---
+++ graphql/resolvers/atendimento.js
@@ -0,0 +1,13 @@
+const Operacoes = require('../../infraestrutura/operations')
+const Atendimentos = new Operacoes('atendimento')
+
+const resolvers = {
+  Query: {
+    atendimentos: () => Atendimentos.lista()
+  },
+  Mutation: {
+    adicionarAtendimento: (root, params) => Atendimentos.adiciona(params)
+  }
+}
+
+module.exports = resolvers
